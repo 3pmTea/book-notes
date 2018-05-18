b79364af7039b470fed6a6fa4d7f43ebd2e23376
3pmtea <3pmtea@gmail.com>
2018-05-18 23:15:45 +0800

    sicp Chapter.2

--- SICP.2nd/solutions/Chapter.2b.rkt
+++ SICP.2nd/solutions/Chapter.2b.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+; ========== pre-defined procedures
+
+(define (square x) (* x x))
+
 ; dispatching utils
 (define *op-table* (make-hash))
 
@@ -9,6 +13,13 @@
 (define (get op type)
   (hash-ref *op-table* (list op type) '()))
 
+; predicates
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
@@ -17,15 +28,6 @@
           (error
            "No method for these types: APPLY-GENERIC"
            (list op type-tags))))))
-
-(define (type-tag x) (car x))
-(define (contents x) (cdr x))
-
-; predicates
-(define (variable? x) (symbol? x))
-(define (same-variable? v1 v2)
-  (and (variable? v1) (variable? v2) (eq? v1 v2)))
-(define (=number? exp num) (and (number? exp) (= exp num)))
 
 ; ========== E2.73
 
@@ -52,7 +54,11 @@
               (deriv (augend exp) var)))
 
   (put 'deriv '+ deriv-sum)
+  (put 'make-sum '+ make-sum)
   'done)
+
+(define (make-sum x y)
+  ((get 'make-sum '+) x y))
 
 (define (install-prod-deriv-package)
   (define (make-product a b)
@@ -64,4 +70,237 @@
   (define (multiplier exp) (car exp))
   (define (multiplicand exp) (cadr exp))
   (define (deriv-prod exp var)
-    (get '
+    (make-sum (make-product (multiplier exp)
+                            (deriv (multiplicand exp) var))
+              (make-product (deriv (multiplier exp) var)
+                            (multiplicand exp))))
+
+  (put 'deriv '* deriv-prod)
+  (put 'make-product '* make-product)
+  'done)
+
+(define (make-product x y)
+  ((get 'make-product '*) x y))
+
+; c
+(define (install-exponent-deriv-package)
+  (define (make-exp base ex)
+    (cond ((not (number? ex)) (error "exponent should be a number, got" ex))
+          ((=number? ex 0) 1)
+          ((=number? ex 1) base)
+          (else (list '** base ex))))
+  (define (base x) (car x))
+  (define (exp x) (cadr x))
+  (define (deriv-exp expression var)
+    (let ((b (base expression))
+          (e (exp expression)))
+      (make-product (make-product e
+                                  (make-exp b (- e 1)))
+                    (deriv b var))))
+
+  (put 'deriv '** deriv-exp)
+  (put 'make-exponent '** make-exp)
+  'done)
+
+; ========== E2.75
+
+(define (make-from-mag-ang-2.75 m a)
+  (lambda (op)
+    (cond ((eq? op 'real-part) (* m (cos a)))
+          ((eq? op 'imag-part) (* m (sin a)))
+          ((eq? op 'magnitude) m)
+          ((eq? op 'angle) a)
+          (else (error "Unknown op: MAKE-FROM-MAG-ANG" op)))))
+
+; ========== E2.78
+
+(define (attach-tag type-tag contents)
+  (cond ((number? contents) contents)
+        (else (cons type-tag contents))))
+
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else (error "Bad tagged datum: TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum: CONTENTS" datum))))
+
+; ========== E2.79 & E2.80
+; scheme-number system
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+  (define (tag x) (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number (lambda (x) (tag x)))
+  ; defined for E2.79
+  (put 'equ? '(scheme-number scheme-number) =)
+  ; defined for E2.80
+  (put '=zero? '(scheme-number)
+       (lambda (x) (= x 0)))
+  'done)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+
+(define (install-rational-package)
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  ; E2.79
+  (put 'equ? '(rational rational)
+       (lambda (x y) (= (* (numer x) (denom y))
+                        (* (denom x) (numer y)))))
+  ; E2.80
+  (put '=zero? '(rational)
+       (lambda (x) (= (numer x) 0)))
+  'done)
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
+(define (install-complex-package)
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  ; E2.79
+  (put 'equ? '(complex complex)
+       (lambda (x y) (and (= (real-part x) (real-part y))
+                          (= (imag-part x) (imag-part y)))))
+  ; E2.80
+  (put '=zero? '(complex)
+       (lambda (x) (= (magnitude x) 0)))
+  'done)
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+(define (install-rectangular-package)
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-polar-package)
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z) (* (magnitude z) (cos (angle z))))
+  (define (imag-part z) (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
+
+; (install-scheme-number-package)
+; (install-rational-package)
+; (install-complex-package)
+; (install-rectangular-package)
+; (install-polar-package)
+
+(define (equ? x y) (apply-generic 'equ? x y))
+(define (=zero? x) (apply-generic '=zero? x))
