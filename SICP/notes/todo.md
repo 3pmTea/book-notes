12290accd1ab0f108b87f81bfc2ea93cd4abcd90
3pmtea <3pmtea@gmail.com>
2018-06-21 22:56:32 +0800

    sicp Chapter 5

--- SICP/notes/todo.md
+++ SICP/notes/todo.md
@@ -1,9 +1,11 @@
-* [Using Closures for Code Generation](http://www.iro.umontreal.ca/~feeley/papers/FeeleyLapalmeCL87.pdf) introduced in Ch 4.1.7
-* [Why Functional Programming Matters](https://www.cs.kent.ac.uk/people/staff/dat/miranda/whyfp90.pdf) and "lazy trees" introduced in Ch 4.2.3
-* [The Conception, Evolution, and Application of Functional Programming Languages](http://haskell.cs.yale.edu/wp-content/uploads/2011/01/cs.pdf)
-* [Logic Programming -- Past, Present and Future](https://www.cs.tufts.edu/~nr/cs257/archive/john-robinson/logic-programming.pdf)
-* [The Japanese National Fifth Generation Project: Introduction, survey and evaluation](https://stacks.stanford.edu/file/druid:kv359wz9060/kv359wz9060.pdf)
-* [Continuation-Based Program Transformation Strategies](http://fsl.cs.illinois.edu/pubs/wand-1980-jacm.pdf) and its [version 2](https://www.cs.indiana.edu/ftp/techreports/TR61.pdf)
+* [Using closures for code generation](http://www.iro.umontreal.ca/~feeley/papers/FeeleyLapalmeCL87.pdf) introduced in Ch 4.1.7
+* [Why functional programming matters](https://www.cs.kent.ac.uk/people/staff/dat/miranda/whyfp90.pdf) and "lazy trees" introduced in Ch 4.2.3
+* [The conception, evolution, and application of functional programming languages](http://haskell.cs.yale.edu/wp-content/uploads/2011/01/cs.pdf)
+* [Logic programming -- past, present and future](https://www.cs.tufts.edu/~nr/cs257/archive/john-robinson/logic-programming.pdf)
+* [The Japanese National Fifth Generation Project: introduction, survey and evaluation](https://stacks.stanford.edu/file/druid:kv359wz9060/kv359wz9060.pdf)
+* [Continuation-based program transformation strategies](http://fsl.cs.illinois.edu/pubs/wand-1980-jacm.pdf) and [another version](https://www.cs.indiana.edu/ftp/techreports/TR61.pdf)
+* [Efficient stack allocation for tail-recursive languages](https://www.cs.purdue.edu/homes/suresh/502-Fall2008/papers/tail-recursion.pdf)
+* [An incremental approach to compiler construction](http://scheme2006.cs.uchicago.edu/11-ghuloum.pdf) introduced on schemewiki
 * [Evaluation strategies](https://en.wikipedia.org/wiki/Evaluation_strategy)
 * [The halting problem](https://en.wikipedia.org/wiki/Halting_problem)
 * [Constraint programming](https://en.wikipedia.org/wiki/Constraint_programming)
--- SICP/solutions/include/compiler.rkt
+++ SICP/solutions/include/compiler.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require "structures.rkt")
 
 (define label-counter 0)
 (define (new-label-number)
@@ -179,3 +180,183 @@
                 (reg env))))
      (compile-sequence (lambda-body exp) 'val 'return))))
 
+(define (compile-application exp target linkage)
+  (let ((proc-code (compile (operator exp) 'proc 'next))
+        (operand-codes
+         (map (lambda
+                  (operand) (compile operand 'val 'next))
+              (operands exp))))
+    (preserving
+     '(env continue) proc-code
+     (preserving
+      '(proc continue) (construct-arglist operand-codes)
+      (compile-procedure-call target linkage)))))
+
+(define (construct-arglist operand-codes)
+  (let ((operand-codes (reverse operand-codes)))
+    (if (null? operand-codes)
+        (make-instruction-sequence
+         '() '(argl)
+         '((assign argl (const ()))))
+        (let ((code-to-get-last-arg
+               (append-instruction-sequences
+                (car operand-codes)
+                (make-instruction-sequence
+                 '(val) '(argl)
+                 '((assign argl (op list) (reg val)))))))
+          (if (null? (cdr operand-codes))
+              code-to-get-last-arg
+              (preserving
+               '(env) code-to-get-last-arg
+               (code-to-get-rest-args (cdr operand-codes))))))))
+
+(define (code-to-get-rest-args operand-codes)
+  (let ((code-for-next-arg
+         (preserving
+          '(argl) (car operand-codes)
+          (make-instruction-sequence
+           '(val argl) '(argl)
+           '((assign argl
+                     (op cons) (reg val) (reg argl)))))))
+    (if (null? (cdr operand-codes))
+        code-for-next-arg
+        (preserving
+         '(env) code-for-next-arg
+         (code-to-get-rest-args (cdr operand-codes))))))
+
+(define (compile-procedure-call target linkage)
+  (let ((primitive-branch (make-label 'primitive-branch))
+        (compiled-branch (make-label 'compiled-branch))
+        (after-call (make-label 'after-call)))
+    (let ((compiled-linkage
+           (if (eq? linkage 'next) after-call linkage)))
+      (append-instruction-sequences
+       (make-instruction-sequence
+        '(proc) '()
+        `((test (op primitive-procedure?) (reg proc))
+          (branch (label ,primitive-branch))))
+       (parallel-instruction-sequences
+        (append-instruction-sequences
+         compiled-branch
+         (compile-proc-appl target compiled-linkage))
+        (append-instruction-sequences
+         primitive-branch
+         (end-with-linkage
+          linkage
+          (make-instruction-sequence
+           '(proc argl) (list target)
+           `((assign ,target
+                     (op apply-primitive-procedure)
+                     (reg proc)
+                     (reg argl)))))))
+       after-call))))
+
+(define all-regs '(env proc val argl continue))
+
+(define (compile-proc-appl target linkage)
+  (cond ((and (eq? target 'val) (not (eq? linkage 'return)))
+         (make-instruction-sequence
+          '(proc) all-regs
+          `((assign continue (label ,linkage))
+            (assign val (op compiled-procedure-entry)
+                    (reg proc))
+            (goto (reg val)))))
+        ((and (not (eq? target 'val))
+              (not (eq? linkage 'return)))
+         (let ((proc-return (make-label 'proc-return)))
+           (make-instruction-sequence
+            '(proc) all-regs
+            `((assign continue (label ,proc-return))
+              (assign val (op compiled-procedure-entry)
+                      (reg proc))
+              (goto (reg val))
+              ,proc-return
+              (assign ,target (reg val))
+              (goto (label ,linkage))))))
+        ((and (eq? target 'val) (eq? linkage 'return))
+         (make-instruction-sequence
+          '(proc continue)
+          all-regs
+          '((assign val (op compiled-procedure-entry)
+                    (reg proc))
+            (goto (reg val)))))
+        ((and (not (eq? target 'val))
+              (eq? linkage 'return))
+         (error "return linkage, target not val: COMPILE"
+                target))))
+
+(define (registers-needed s)
+  (if (symbol? s) '() (car s)))
+(define (registers-modified s)
+  (if (symbol? s) '() (cadr s)))
+(define (statements s)
+  (if (symbol? s) (list s) (caddr s)))
+
+(define (needs-register? seq reg)
+  (memq reg (registers-needed seq)))
+(define (modifies-register? seq reg)
+  (memq reg (registers-modified seq)))
+
+(define (append-instruction-sequences . seqs)
+  (define (append-2-sequences seq1 seq2)
+    (make-instruction-sequence
+     (list-union
+      (registers-needed seq1)
+      (list-difference (registers-needed seq2)
+                       (registers-modified seq1)))
+     (list-union (registers-modified seq1)
+                 (registers-modified seq2))
+     (append (statements seq1) (statements seq2))))
+  (define (append-seq-list seqs)
+    (if (null? seqs)
+        (empty-instruction-sequence)
+        (append-2-sequences
+         (car seqs)
+         (append-seq-list (cdr seqs)))))
+  (append-seq-list seqs))
+
+(define (list-union s1 s2)
+  (cond ((null? s1) s2)
+        ((memq (car s1) s2) (list-union (cdr s1) s2))
+        (else (cons (car s1) (list-union (cdr s1) s2)))))
+
+(define (list-difference s1 s2)
+  (cond ((null? s1) '())
+        ((memq (car s1) s2) (list-difference (cdr s1) s2))
+        (else (cons (car s1)
+                    (list-difference (cdr s1) s2)))))
+
+(define (preserving regs seq1 seq2)
+  (if (null? regs)
+      (append-instruction-sequences seq1 seq2)
+      (let ((first-reg (car regs)))
+        (if (and (needs-register? seq2 first-reg)
+                 (modifies-register? seq1 first-reg))
+            (preserving
+             (cdr regs)
+             (make-instruction-sequence
+              (list-union (list first-reg)
+                          (registers-needed seq1))
+              (list-difference (registers-modified seq1)
+                               (list first-reg))
+              (append `((save ,first-reg))
+                      (statements seq1)
+                      `((restore ,first-reg))))
+             seq2)
+            (preserving (cdr regs) seq1 seq2)))))
+
+(define (tack-on-instruction-sequence seq body-seq)
+  (make-instruction-sequence
+   (registers-needed seq)
+   (registers-modified seq)
+   (append (statements seq)
+           (statements body-seq))))
+
+(define (parallel-instruction-sequences seq1 seq2)
+  (make-instruction-sequence
+   (list-union (registers-needed seq1)
+               (registers-needed seq2))
+   (list-union (registers-modified seq1)
+               (registers-modified seq2))
+   (append (statements seq1)
+           (statements seq2))))
